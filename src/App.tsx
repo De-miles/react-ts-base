4457f735177aea23f924aefbb6a2997c7145b630
De-miles <miles998321@gmail.com>
2024-07-24 16:52:09 +0700

    practice Props p1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import './App.css'
 import { Footer, Header } from './components/common'
+import { Student } from './features/labs/Student'
 
 function App() {
+
+  function abc() { }
 
   return (
     <>
       <Header></Header>
+      <Student
+        name='truong'
+        age={18}
+        isHero
+        hobbyList={['eat', 'code', 'sleep']}
+        sayHello={abc} />
       <Footer></Footer>
     </>
   )
--- src/features/labs/Student.tsx
+++ src/features/labs/Student.tsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
 
 export interface StudentProps {
+  name?: string
+  age: number
+  isHero?: boolean
+  hobbyList: string[]
+  sayHello: () => void
 }
 
-export function Student() {
+export function Student({ name = 'truong', isHero = false }: StudentProps) {
   return (
     <div>
-      Student feature
+      Student: {name} {isHero ? 'Hero' : 'No-Hero'}
     </div>
   );
 }
